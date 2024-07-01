6774dd73e78a5296773176fad8d97c15ae70f470
programmers-union <github@programmersunion.in>
2024-07-01 10:54:46 +0530

    hi

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App: React.FC = () => {
   return (
     <div className='flex justify-center items-center h-screen m-auto bg-[#fffcf2]'>
-      <h1 className='bg-blue-500 px-4 py-1 rounded-lg text-white hover:bg-slate-900 shadow-md cursor-pointer'>Hi👋</h1>
+      <h1 className='bg-blue-500 px-4 py-1 rounded-lg text-white hover:bg-slate-900 shadow-md cursor-pointer'>Hiiiiiiiii👋</h1>
     </div>
   );
 }
